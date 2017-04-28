a8b5ae86aa6b55382395493768a752363d9a6e92
hawkinfinite <526273428@qq.com>
2017-04-28 15:26:40 +0800

    add 电影评论匹配作业

---
+++ 电影评论匹配作业/Hello World.py
@@ -0,0 +1,59 @@
+#encoding=utf-8
+
+import jieba
+import codecs
+
+Dict = []    #个人词典
+lines = ""   #初始化lines
+
+#把提供的词典加入自己的词典中
+def match(pathOfTxt):
+    with codecs.open(pathOfTxt, 'r', 'utf-8') as DictFile:
+        for line in DictFile.readlines():
+            line = line.strip('\n')    #去除\n
+            Dict.append((line, 0))
+
+#麻烦师兄/师姐自己改下路径啦= =
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/角色/反派.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/角色/角色.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/角色/角色中的其它.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/角色/男主角.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/角色/配角.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/角色/熊顿.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/剧情/剧情.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/剧情/开头.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/剧情/发展.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/剧情/结局.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/剧情/笑点.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/剧情/泪点.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/视听/视听.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/视听/视听效果中的其它.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/视听/画面.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/视听/镜头.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/视听/动作.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/视听/音乐.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/制作/制作.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/制作/导演.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/制作/编剧.txt')
+#match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/制作/选景.txt')  为什么选景这个txt无法打开？？？
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/制作/出品公司.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/主题/风格.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/主题/主题.txt')
+match('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/词典/主题/题材内容.txt')
+
+with codecs.open('/home/infinite/Downloads/2017数据挖掘作业/week2 Python 作业/作业/太空旅店.txt', 'r', 'utf-8') as filein:
+    for line in filein.readlines():
+        line.strip('\n')
+        lines += line
+
+ItemsDict = dict(Dict)
+words = jieba.cut(lines)
+fileOu = codecs.open('/home/infinite/my_github/PythonStudy/匹配结果.txt', 'w', 'utf-8')
+
+for word in words:
+    if word in ItemsDict.keys():
+        ItemsDict[word] += 1
+
+for k in ItemsDict.keys():
+    ou = k + ' ' + str(ItemsDict[k]) + '\n'
+    fileOu.write(ou)
